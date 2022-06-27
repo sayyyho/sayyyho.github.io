37394f46ba26c2682d2a11e1cd5919f63785d24b
sayyyho <323psh@naver.com>
2022-06-27 15:04:20 +0900

    add clock

---
+++ clock.js
@@ -0,0 +1,12 @@
+const clock = document.querySelector("h4#clock")
+
+function showClock(){
+    const date = new Date();
+    const hours = String(date.getHours()).padStart(2,"0");
+    const minutes = String(date.getMinutes()).padStart(2,"0");
+    const seconds = String(date.getSeconds()).padStart(2, "0");
+    clock.innerHTML = `${hours}:${minutes}:${seconds}`;
+}
+
+showClock();
+setInterval(showClock,"1000");
